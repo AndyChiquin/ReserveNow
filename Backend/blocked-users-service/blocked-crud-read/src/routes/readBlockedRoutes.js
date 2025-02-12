6ad57ba2336c37d841cc408280975d806cc657d5
Kevin Eduardo Anchali Zumba <85031952+KEvinEdu14@users.noreply.github.com>
2025-02-12 16:09:58 -0500

    Delete Backend/blocked-users-service directory

--- Backend/blocked-users-service/blocked-crud-read/src/routes/readBlockedRoutes.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const { isUserBlocked } = require('../controllers/readBlockedController');
-
-const router = express.Router();
-
-router.get('/:userId', isUserBlocked);
-
-module.exports = router;
